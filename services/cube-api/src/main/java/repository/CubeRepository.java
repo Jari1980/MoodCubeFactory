a9042678c805e7257848e7d34800924e88fdef01
Jari1980 <jari.tenhunen@outlook.com>
2026-06-15 11:56:50 +0200

    getCube and UpdateCube added, tested with Postman

--- services/cube-api/src/main/java/repository/CubeRepository.java
+++ services/cube-api/src/main/java/repository/CubeRepository.java
@@ -3,7 +3,10 @@ package repository;
 import model.Cube;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CubeRepository {
     List<Cube> getAll();
+    Optional<Cube> getById(int id);
+    Cube update(Cube cube);
 }
